40531f87811368909a4e6d6929a9a03f37ce8524
Paul Meier <paulmartinmeier@gmail.com>
2018-11-22 12:10:43 -0600

    Added yargs functionality for commands.

--- app.js
+++ app.js
@@ -3,7 +3,31 @@ const _ = require("lodash");
 const yargs = require("yargs");
 
 const notes = require("./notes.js");
-const argv = yargs.argv;
+
+const titleOptions = {
+  describe: "Title of a note",
+  demand: true,
+  alias: 't'
+}
+const bodyOptions = {
+  describe: "A note body",
+  demand: true,
+  alias: 'b'
+}
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
 
 var command = argv._[0];
 
